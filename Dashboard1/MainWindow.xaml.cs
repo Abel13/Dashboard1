286870302029c9415b8deee6f4400f55675c05c5
Abel <abel.o.d@outlook.com>
2017-12-04 17:43:08 -0200

    Primeiro Commit

---
+++ Dashboard1/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dashboard1
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Consumo consumo = new Consumo();
+            DataContext = new ConsumoViewModel(consumo);
+        }
+
+        private void ButtonFechar_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void GridBarraTitulo_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+
+    }
+
+    internal class ConsumoViewModel
+    {
+        public List<Consumo> Consumo { get; private set; }
+
+        public ConsumoViewModel(Consumo consumo)
+        {
+            Consumo = new List<Consumo>();
+            Consumo.Add(consumo);
+        }
+    }
+
+    internal class Consumo
+    {
+        public string Titulo { get; private set; }
+        public int Porcentagem { get; private set; }
+
+        public Consumo()
+        {
+            Titulo = "Consumo Atual";
+            Porcentagem = CalcularPorcentagem();
+        }
+
+        private int CalcularPorcentagem()
+        {
+            return 47; //Calculo da porcentagem de consumo
+        }
+    }
+}
